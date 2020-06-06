e21bf56e0ad80d4b2c3a9523d97a3082aa46229e
deliadancer@gmail.com <ceige@dorian.localdomain>
2020-06-06 00:28:46 -0400

    initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_UYbLBcAT8',
+        userPoolClientId: '6vhsr3qgfe7uoolbun2uilmnjk',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
