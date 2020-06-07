75465fc413b96d07cec5009325b6016a88853e1a
Cordelia <deliadancer@gmail.com>
2020-06-07 12:16:03 -0400

    Fix a weird auth thing

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -121,7 +121,7 @@ var DictionaryGame = window.DictionaryGame || {};
         signin(email, password,
             function signinSuccess() {
                 console.log('Successfully Logged In');
-                window.location.href = 'ride.html';
+                window.location.href = 'game.html';
             },
             function signinError(err) {
                 alert(err);
